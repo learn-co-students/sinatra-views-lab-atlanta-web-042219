83a6ecb7c4f62249ccd83ebb7f188501e0397710
Preston Elliott <preston.elliott24@gmail.com>
2019-05-13 15:58:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,39 @@
+# class App < Sinatra::Base
+
+# 	# get '/' do
+# 	# 	erb :index
+# 	# end
+
+# 	get '/hello' do
+# 		erb :hello
+# 	end
+
+# 	get '/goodbye' do
+# 		erb :goodbye
+# 	end
+
+# 	get '/date' do
+# 		erb :date
+# 	end
+
+# end
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
